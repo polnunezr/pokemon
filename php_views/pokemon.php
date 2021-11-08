8dd1c172898b5acf042afb62bd46efc0f7e9066d
Pol Nuñez <pnunezr2021@cepnet.net>
2021-11-08 13:10:34 +0100

    Revisar formulario

--- php_views/pokemon.php
+++ php_views/pokemon.php
@@ -1,52 +1,110 @@
-<!DOCTYPE html>
-<html lang="en">
+    <!DOCTYPE html>
+    <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Formulari Pokemon</title>
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Formulari Pokemon</title>
+        <?php
+        include '../bootstrap/index.php';
+        ?>
+    </head>
 
-<body>
-    <label for="txtNumero">Número</label>
-    <input type="text" name="txtNumero" id="txtNumero" maxlength="3" placeholder="Número del pokémon" autofocus><br>
-    <label for="txtNombre">Nombre</label>
-    <input type="text" name="txtNombre" id="txtNombre" placeholder="Nombre del pokémon"><br>
-    <label for="region">Región</label>
-    <select name="region" id="region">
-        <option value="kanto">Kanto</option>
-        <option value="johto">Johto</option>
-        <option value="hoenn">Hoenn</option>
-        <option value="sinnoh">Sinnoh</option>
-        <option value="teselia">Teselia</option>
-    </select><br>
-    <label for="tipo">Tipo</label>
-    <input type="checkbox" name="tipoPokemon[]" id="planta" value="planta">Planta
-    <input type="checkbox" name="tipoPokemon[]" id="veneno" value="veneno">Veneno
-    <input type="checkbox" name="tipoPokemon[]" id="fuego" value="fuego">Fuego
-    <input type="checkbox" name="tipoPokemon[]" id="volador" value="volador">Volador
-    <input type="checkbox" name="tipoPokemon[]" id="agua" value="agua">Agua
-    <input type="checkbox" name="tipoPokemon[]" id="electrico" value="electrico">Eléctrico
-    <input type="checkbox" name="tipoPokemon[]" id="hada" value="hada">Hada
-    <input type="checkbox" name="tipoPokemon[]" id="bicho" value="bicho">Bicho
-    <input type="checkbox" name="tipoPokemon[]" id="lucha" value="lucha">Lucha
-    <input type="checkbox" name="tipoPokemon[]" id="psiquico" value="psiqiuco">Psíquico <br>
-    <label for="altura">Altura</label>
-    <input type="number" name="altura" id="altura" min="1"><br>
-    <label for="peso">Peso</label>
-    <input type="number" min="0" step=".01" name="peso" id="peso"><br>
-    <label for="evolucion">Evolución</label>
-    <input type="radio" name="rEvolucion" id="rbSinEvolucionar" value="rbSinEvolucionar">
-    <label for="rbSinEvolucionar">Sin Evolucionar</label>
-    <input type="radio" name="rEvolucion" id="rbPrimeraEvolucion" value="rbPrimeraEvolucion">
-    <label for="rbPrimeraEvolucion">Primera evolución</label>
-    <input type="radio" name="rEvolucion" id="rbSegundaEvolucion" value="rbSegundaEvolucion">
-    <label for="rbSegundaEvolucion">Segunda evolución</label><br>
-    <label for="imagen">Imagen</label>
-    <input type="file" id="imagen" name="imagen" accept="image/png, image/jpeg"><br>
-    <button type="submit">Aceptar</button> <a href=""> Cancelar</a>
+    <body>
+        <div class="container-fluid" style="width: 45%;height:55%;margin-top:200px">
 
-</body>
+            <form class="border border-dark justify-content-center">
+                <header class="card-header"><img src="../media/pokeball.png" height="20">Pokemon</header>
+                <div class="form-group row">
+                    <label for="txtNumero" class="col-sm-2 col-form-label">Número:</label>
+                    <div class="col-sm-10">
+                        <input class="form-control w-75" type="text" name="txtNumero" id="txtNumero" maxlength="3" placeholder="Número del pokémon" autofocus><br>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="txtNombre" class="col-sm-2 col-form-label">Nombre</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control w-75" name="txtNombre" id="txtNombre" placeholder="Nombre del pokémon">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="region" class="col-sm-2 col-form-label">Región</label>
+                    <div class="col-sm-10">
+                        <select name="region" id="region" class="form-control w-75">
+                            <option value="kanto">Kanto</option>
+                            <option value="johto">Johto</option>
+                            <option value="hoenn">Hoenn</option>
+                            <option value="sinnoh">Sinnoh</option>
+                            <option value="teselia">Teselia</option>
+                        </select>
+                    </div>
+                </div>
 
-</html>
+                <div class="form-group row">
+                    <div class="col-sm-2">Tipo</div>
+                    <div class="col-sm-10">
+                        <div class="form-check">
+                            <label for="tipo" class="form-check-label">
+                                <input type="checkbox" name="tipoPokemon[]" id="planta" value="planta">Planta
+                                <input type="checkbox" name="tipoPokemon[]" id="veneno" value="veneno">Veneno
+                                <input type="checkbox" name="tipoPokemon[]" id="fuego" value="fuego">Fuego
+                                <input type="checkbox" name="tipoPokemon[]" id="volador" value="volador">Volador
+                                <input type="checkbox" name="tipoPokemon[]" id="agua" value="agua">Agua
+                                <input type="checkbox" name="tipoPokemon[]" id="electrico" value="electrico">Eléctrico
+                                <input type="checkbox" name="tipoPokemon[]" id="hada" value="hada">Hada
+                                <input type="checkbox" name="tipoPokemon[]" id="bicho" value="bicho">Bicho
+                                <input type="checkbox" name="tipoPokemon[]" id="lucha" value="lucha">Lucha
+                                <input type="checkbox" name="tipoPokemon[]" id="psiquico" value="psiqiuco">Psíquico <br>
+                            </label>
+                        </div>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <div class="col-sm-10">
+                        <div class="input-group mb-3">
+                            <label for="peso" class="col-sm-2 col-form-label">Altura</label>
+                            <input type="number" class="form-control w-50" name="altura" id="altura" min="1" style="margin-left: 4%;">
+                            <span class="input-group-text" style="margin-right:5%">cm</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <div class="col-sm-10">
+                        <div class="input-group mb-3">
+                            <label for="peso" class="col-sm-2 col-form-label">Peso</label>
+                            <input type="number" class="form-control w-50" min="0" step=".01" name="peso" id="peso" style="margin-left: 4%;">
+                            <span class="input-group-text" style="margin-right:5%">KG</span>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="form-group row">
+                    <div class="col-sm-2">Evolución</div>
+                    <div class="col-sm-10">
+
+                        <input type="radio" class="form-check-input" name="rEvolucion" id="rbSinEvolucionar" value="rbSinEvolucionar">
+                        <label for="rbSinEvolucionar" class="form-check-label">Sin Evolucionar</label>
+                        <input type="radio" class="form-check-input" name="rEvolucion" id="rbPrimeraEvolucion" value="rbPrimeraEvolucion">
+                        <label for="rbPrimeraEvolucion" class="form-check-label">Primera evolución</label>
+                        <input type="radio" class="form-check-input" name="rEvolucion" id="rbSegundaEvolucion" value="rbSegundaEvolucion">
+                        <label for="rbSegundaEvolucion" class="form-check-label">Segunda evolución</label>
+                    </div>
+                </div>
+
+                <div class="form-group row">
+                    <label for="imagen" class="col-sm-2 col-form-label">Imagen</label>
+                    <div class="col-sm-10">
+                        <input type="file" id="imagen" name="imagen" accept="image/png, image/jpeg">
+
+                    </div>
+                </div>
+
+                    <a class="btn btn-secondary text-end" href="\pokemon\index.php" role="button">Cancelar</a>
+                    <button type="submit" class="btn btn-primary text-end">Aceptar</button>
+            </form>
+            
+        </div>
+    </body>
+
+    </html>
--- php_views/pokemon_list.php
+++ php_views/pokemon_list.php
@@ -3,8 +3,8 @@ include '../bootstrap/index.php';
 include '../php_partials/menu.php';
 ?>
 <div class="container-fluid">
-    <div class="row"style="margin-top: 20px;">
-        <div class="col">
+    <div class="row row-cols-1 row-cols-md-5" >
+        <div class="col mt-2">
             <div class="card" style="width: 18rem;">
                 <span class="border border-secondary">
                     <img src="/pokemon/media/001.png" class="card-img-top">
@@ -12,8 +12,6 @@ include '../php_partials/menu.php';
                         <h5 class="card-title">001-Bulbasur</h5>
                         <span class="badge bg-warning text-dark">Planta</span>
                         <span class="badge bg-warning text-dark">Veneno</span>
-
-
                     </div>
                     <footer class="card-footer  text-end">
                         <button type="submit" class="btn btn-outline-danger"><i class="fas fa-trash-alt"></i></button>
@@ -22,7 +20,7 @@ include '../php_partials/menu.php';
                 </span>
             </div>
         </div>
-        <div class="col">
+        <div class="col mt-2">
             <div class="card" style="width: 18rem;">
                 <span class="border border-secondary">
                     <img src="/pokemon/media/002.png" class="card-img-top" alt="...">
@@ -33,13 +31,13 @@ include '../php_partials/menu.php';
 
                     </div>
                     <footer class="card-footer  text-end">
-                        <button type="submit" class="btn btn-outline-danger"><i class="fas fa-trash-alt"></i></button>
-                        <button type="submit" class="btn btn-outline-primary"><i class="fas fa-edit"></i></button>
+                        <button type="submit" class="btn btn-outline-danger" id="borrar"><i class="fas fa-trash-alt"></i></button>
+                        <button type="submit" class="btn btn-outline-primary" id="editar"><i class="fas fa-edit"></i></button>
                     </footer>
                 </span>
             </div>
         </div>
-        <div class="col ">
+        <div class=" col mt-2">
             <div class="card" style="width: 18rem;">
                 <span class="border border-secondary">
                     <img src="/pokemon/media/003.png" class="card-img-top" alt="...">
@@ -56,7 +54,7 @@ include '../php_partials/menu.php';
                 </span>
             </div>
         </div>
-        <div class="col">
+        <div class=" col mt-2">
             <div class="card" style="width: 18rem;">
                 <span class="border border-secondary">
                     <img src="/pokemon/media/004.png" class="card-img-top" alt="...">
@@ -72,7 +70,7 @@ include '../php_partials/menu.php';
                 </span>
             </div>
         </div>
-        <div class="col">
+        <div class=" col mt-2">
             <div class="card" style="width: 18rem;">
                 <span class="border border-secondary">
                     <img src="/pokemon/media/005.png" class="card-img-top" alt="...">
@@ -88,9 +86,76 @@ include '../php_partials/menu.php';
                 </span>
             </div>
         </div>
-        <div class=" fixed-bottom " style="margin-bottom:10px;z-index:100;"><button class="btn btn-secondary float-right">Button</button></div><br>
-
     </div>
-   
+    <div class="row row-cols-1 row-cols-md-5  ">
+        <div class=" col mt-2">
+            <div class="card" style="width: 18rem;">
+                <span class="border border-secondary">
+                    <img src="/pokemon/media/006.png" class="card-img-top">
+                    <div class="card-body">
+                        <h5 class="card-title">006-Charizard</h5>
+                        <span class="badge bg-warning text-dark">Fuego</span>
+                    </div>
+                    <footer class="card-footer  text-end">
+                        <button type="submit" class="btn btn-outline-danger"><i class="fas fa-trash-alt"></i></button>
+                        <button type="submit" class="btn btn-outline-primary"><i class="fas fa-edit"></i></button>
+                    </footer>
+                </span>
+            </div>
+        </div>
+        <div class=" col mt-2">
+            <div class="card" style="width: 18rem;">
+                <span class="border border-secondary">
+                    <img src="/pokemon/media/007.png" class="card-img-top">
+                    <div class="card-body">
+                        <h5 class="card-title">007-Squirtle</h5>
+                        <span class="badge bg-warning text-dark">Agua</span>
+                    </div>
+                    <footer class="card-footer  text-end">
+                        <button type="submit" class="btn btn-outline-danger"><i class="fas fa-trash-alt"></i></button>
+                        <button type="submit" class="btn btn-outline-primary"><i class="fas fa-edit"></i></button>
+                    </footer>
+                </span>
+            </div>
+        </div>
+        <div class=" col mt-2">
+            <div class="card" style="width: 18rem;">
+                <span class="border border-secondary">
+                    <img src="/pokemon/media/008.png" class="card-img-top">
+                    <div class="card-body">
+                        <h5 class="card-title">008-Wartortle</h5>
+                        <span class="badge bg-warning text-dark">Agua</span>
+                    </div>
+                    <footer class="card-footer  text-end">
+                        <button type="submit" class="btn btn-outline-danger"><i class="fas fa-trash-alt"></i></button>
+                        <button type="submit" class="btn btn-outline-primary"><i class="fas fa-edit"></i></button>
+                    </footer>
+                </span>
+            </div>
+        </div>
+        <div class=" col mt-2">
+            <div class="card" style="width: 18rem;">
+                <span class="border border-secondary">
+                    <img src="/pokemon/media/009.png" class="card-img-top">
+                    <div class="card-body">
+                        <h5 class="card-title">009-Blastoise</h5>
+                        <span class="badge bg-warning text-dark">Agua</span>
+                    </div>
+                    <footer class="card-footer  text-end">
+                        <button type="submit" class="btn btn-outline-danger"><i class="fas fa-trash-alt"></i></button>
+                        <button type="submit" class="btn btn-outline-primary"><i class="fas fa-edit"></i></button>
+                    </footer>
+                </span>
+            </div>
+        </div>
+    </div>
+
+    <div class="position-bottom position-fixed bottom-0 end-0 m-5" style="height:20px;width:2px;">
+        <a href="pokemon.php" type="button" class="btn bg-warning text-dark rounded-circle"><i class="fas fa-plus"></i></a>
+    </div>
+
 
 </div>
+</body>
+
+</html>
